1340a883634f55b115a6e911fd1d732797ae4333
ivancartolano <ivanscaranello@gmail.com>
2017-10-10 23:11:46 -0300

    Steven adando pra baixo

--- character.lua
+++ character.lua
@@ -1,5 +1,10 @@
 local character = {}
 
+	local frames = {}
+	local activeFrame
+	local currentFrame = 1
+	local currentY = 0
+
 	local bitmap = {
 		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,1},
@@ -15,20 +20,52 @@ local character = {}
 		
 	
 	function character:carregar(x,y,imageFile)
-		local frames = {}
 		
 		frames[1] = love.graphics.newQuad(x*32,y*32,32,32, imageFile:getDimensions())
-		frames[2] = love.graphics.newQuad(x*32,(y+1)*32,32,32, imageFile:getDimensions())
-		frames[3] = love.graphics.newQuad(x*32,(y+2)*32,32,32, imageFile:getDimensions())
-		frames[4] = love.graphics.newQuad((x+1)*32,32,32,32, imageFile:getDimensions())
+		frames[2] = love.graphics.newQuad((x+1)*32,y*32,32,32, imageFile:getDimensions())
+		frames[3] = love.graphics.newQuad((x+2)*32,y*32,32,32, imageFile:getDimensions())
+		frames[4] = love.graphics.newQuad(x*32,(y+1)*32,32,32, imageFile:getDimensions())
 		frames[5] = love.graphics.newQuad((x+1)*32,(y+1)*32,32,32, imageFile:getDimensions())
-		frames[6] = love.graphics.newQuad((x+1)*32,(y+2)*32,32,32, imageFile:getDimensions())
-		frames[7] = love.graphics.newQuad((x+2)*32,32,32,32, imageFile:getDimensions())
-		frames[8] = love.graphics.newQuad((x+2)*32,(y+1)*32,32,32, imageFile:getDimensions())
+		frames[6] = love.graphics.newQuad((x+2)*32,(y+1)*32,32,32, imageFile:getDimensions())
+		frames[7] = love.graphics.newQuad(x*32,(y+2)*32,32,32, imageFile:getDimensions())
+		frames[8] = love.graphics.newQuad((x+1)*32,(y+2)*32,32,32, imageFile:getDimensions())
 		frames[9] = love.graphics.newQuad((x+2)*32,(y+2)*32,32,32, imageFile:getDimensions())
-		frames[10] = love.graphics.newQuad((x+3)*32,32,32,32, imageFile:getDimensions())
-		frames[11] = love.graphics.newQuad((x+3)*32,(y+1)*32,32,32, imageFile:getDimensions())
-		frames[12] = love.graphics.newQuad((x+3)*32,(y+2)*32,32,32, imageFile:getDimensions())
+		frames[10] = love.graphics.newQuad(x*32,(y+3)*32,32,32, imageFile:getDimensions())
+		frames[11] = love.graphics.newQuad((x+2)*32,(y+3)*32,32,32, imageFile:getDimensions())
+		frames[12] = love.graphics.newQuad((x+2)*32,(y+3)*32,32,32, imageFile:getDimensions())
 		
-		return frames
+		activeFrame = frames[currentFrame]
+		
 	end
+	
+	local elapsedTime = 0
+	
+	function character:moveDown(dt)
+		elapsedTime = elapsedTime +dt
+	
+		if(elapsedTime > 0.25) then
+			if (currentFrame < 3) then
+				currentFrame = currentFrame + 1
+			else
+				currentFrame = 1
+			end
+			activeFrame = frames[currentFrame]
+			elapsedTime = 0
+		end
+	
+		if (currentY < love.graphics.getHeight()) then
+			currentY = currentY + 1
+		else
+			currentY = 0
+		end
+	end
+	
+	--function character:atualizar()
+	
+	--end
+	
+	function character:desenhar(imageFile)
+		love.graphics.draw(imageFile, activeFrame, 0, currentY)
+	end
+	
+return character
